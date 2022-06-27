8864b31e910b73ad6ffae2b3b900af2a88d6ca20
kimjw2003 <55072002+kimjw2003@users.noreply.github.com>
2022-06-28 00:20:05 +0900

    add: detail view 작업 준비

---
+++ app/src/main/java/com/example/wanted/view/detail/DetailFragment.kt
@@ -0,0 +1,34 @@
+package com.example.wanted.view.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import com.example.wanted.R
+import com.example.wanted.databinding.FragmentDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class DetailFragment : Fragment() {
+
+    private lateinit var binding : FragmentDetailBinding
+    private val viewModel : DetailViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(binding) {
+            setVariable(0, viewModel)
+            vm = viewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+    }
+}
