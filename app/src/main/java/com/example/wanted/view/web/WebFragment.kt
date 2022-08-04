e5cf1dbcc4da2601e26f708459c919d03eb1b61a
kimjw2003 <55072002+kimjw2003@users.noreply.github.com>
2022-08-04 09:31:04 +0900

    fix: webView내에서 뒤로가기 활동 제어

--- app/src/main/java/com/example/wanted/view/web/WebFragment.kt
+++ app/src/main/java/com/example/wanted/view/web/WebFragment.kt
@@ -2,6 +2,7 @@ package com.example.wanted.view.web
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -39,7 +40,18 @@ class WebFragment : Fragment() {
                 urlInfo = arguments?.getString("webUrlInfo") as String
 
                 loadUrl(urlInfo)
+
+                setOnKeyListener(View.OnKeyListener{ _, keyCode, _ ->
+                    if(keyCode == KeyEvent.KEYCODE_BACK && canGoBack()) {
+                        goBack()
+                        return@OnKeyListener true
+                    }
+
+                    return@OnKeyListener false
+                })
             }
         }
     }
+
+
 }
