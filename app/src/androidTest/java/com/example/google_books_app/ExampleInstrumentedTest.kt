8fd931b75cd9c9ff0652be39fcdd080431c3abea
kimjw2003 <55072002+kimjw2003@users.noreply.github.com>
2022-08-08 09:04:15 +0900

    fix: gradle

--- app/src/androidTest/java/com/example/google_books_app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/google_books_app/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.wanted", appContext.packageName)
+        assertEquals("com.example.google_books_app", appContext.packageName)
     }
 }
